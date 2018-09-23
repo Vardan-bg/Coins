c9af0e20b2ae04df322d005ed6172b8f6871fa68
Vardan-bg <vartan.bg@gmail.com>
2018-09-23 20:08:14 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# coins
+
+> Coins game
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm start
+
+# build for production
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-typed](https://github.com/budiadiono/vue-typed).
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+var path = require('path')
+var webpack = require('webpack')
+var outputFileName = 'bundle'
+
+var config = {
+  context: path.resolve(__dirname, './'),
+
+  entry: ['./src/index.ts'],
+
+  output: {
+    // output to './dist' folder 
+    path: path.resolve(__dirname, 'dist'),
+
+    // with filename
+    filename: outputFileName + '.js',
+
+    // mark /dist/ folder as a public path so index.html can reach it
+    publicPath: '/dist/'
+  },
+
+  // webpack-dev-server config, see: https://webpack.github.io/docs/webpack-dev-server.html
+  devServer: {
+    contentBase: './',
+    hot: true,
+    inline: true,
+    port: 8080
+  },
+
+  // http://webpack.github.io/docs/configuration.html#devtool
+  devtool: '#eval-source-map',
+
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: 'html-loader' }
+    ]
+  },
+
+  plugins: [
+    // HMR issue, see: https://github.com/webpack/webpack/issues/1151
+    new webpack.HotModuleReplacementPlugin()
+  ]
+
+}
+
+// When use in production (npm run build)
+if (process.env.NODE_ENV === 'production') {
+
+  // You may want to use different name for production
+  // config.output.filename = outputFileName + '.min.js'
+  
+  // still need babel for production stage since uglifyJs not support es6
+  config.module.loaders = (config.module.loaders || []).concat([
+    { test: /\.ts(x?)$/, loader: 'babel-loader?presets[]=es2015!ts-loader' },
+    { test: /\.js$/, loader: 'babel-loader', query: { presets: ['es2015'] } }
+  ])
+
+  config.devtool = '#source-map'
+
+  // https://vuejs.org/guide/deployment.html
+  config.plugins = (config.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ])
+
+} else {
+
+  config.module.loaders = config.module.loaders.concat([
+    { test: /\.ts(x?)$/, loader: 'ts-loader' }
+  ])
+
+}
+
+
+module.exports = config
