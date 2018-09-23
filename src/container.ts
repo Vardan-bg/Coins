5b9be54124345d00b82ffee0730a22e305a1734c
Vardan-bg <vartan.bg@gmail.com>
2018-09-23 22:19:05 +0400

    coins

--- src/container.ts
+++ src/container.ts
@@ -1,10 +1,14 @@
 import { Component, Prop  } from 'vue-typed'
 import Vue from 'vue'
+import { Coin } from './coin/coin'
+
+Vue.component('coin', Coin)
 
 @Component({
 	template: require('./container.html')
 })
 export class Container extends Vue {
 	@Prop({ type: String })
-	title: string
+	title: string;
+	@Prop({ type: Array, default: function () { return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] }}) coins: Array<number>;
 }
